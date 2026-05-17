c5c73e787e4c65d6f38a925a49da9fb1cd08ce5e
KacperJodjod <kacperosj2003@gmail.com>
2026-05-17 15:49:38 +0200

    Adjusted main to new interfaces

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import numpy as np
 from EuropeanOptions import *
 from MarketModel import MarketModel
 
-BinomialMarketModel = MarketModel(SpotPrice=50, up=1.1, down=0.9, risk_free_rate=0.02, delta_T=1/12, max_maturity=2)
-
-EuropeanCallOption = EuropeanCallOption(Strike=48, Maturity=2)
-
-MarketModel.priceOption(EuropeanCallOption)
-
-
-
-
 S0 = 50
 K = 48
 T = 2
@@ -19,15 +10,25 @@ r = 0.02
 dt = 1 / 12
 
 
-call = european_option_binomial(S0, K, T, r, sigma, dt, "call")
-put = european_option_binomial(S0, K, T, r, sigma, dt, "put")
+BinomialMarketModel = MarketModel(SpotPrice = 50, 
+                                  up = np.exp(sigma * np.sqrt(dt)),
+                                  down = np.exp((-1) * sigma * np.sqrt(dt)), 
+                                  risk_free_rate = 0.02,
+                                  delta_T = 1 / 12, 
+                                  max_maturity = 2 )
 
-print("European Call:", call)
-print("European Put :", put)
+E_Call = EuropeanCallOption(Strike = 48, Maturity = 2)
+E_Put = EuropeanPutOption(Strike = 48, Maturity = 2)
+
+E_Call_Value = MarketModel.priceOption(E_Call)
+E_Put_Value = MarketModel.priceOption(E_Put)
+
+print("European Call:", E_Call_Value)
+print("European Put :", E_Put_Value)
 
 # sprawdzenie wyników parytetem put - call
 
 print("\nCall-put parity:")
-print("C - P =", call - put)
+print("C - P =", E_Call_Value - E_Put_Value)
 print("S0 - K*exp(-rT) =", S0 - K * np.exp(-r * T))
-print("Difference =", abs((call - put) - (S0 - K * np.exp(-r * T))))
+print("Difference =", abs((E_Call_Value - E_Put_Value) - (S0 - K * np.exp(-r * T))))
