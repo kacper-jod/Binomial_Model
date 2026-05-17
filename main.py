9a4bf54651413fc6a33f35f27b5ac07fea5b9885
KacperJodjod <kacperosj2003@gmail.com>
2026-05-17 16:58:25 +0200

    Changing dt to 1/4, because the tree is too big for now

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ K = 48
 T = 2
 sigma = 0.3
 r = 0.02
-dt = 1 / 12
+dt = 1 / 4 #changed from 1/12, its too much for now, we have too big tree
 
-
-BinomialMarketModel = MarketModel(SpotPrice = 50, 
+BinomialMarketModel = MarketModel(SpotPrice = S0, 
                                   up = np.exp(sigma * np.sqrt(dt)),
                                   down = np.exp((-1) * sigma * np.sqrt(dt)), 
-                                  risk_free_rate = 0.02,
-                                  delta_T = 1 / 12, 
-                                  max_maturity = 2 )
+                                  risk_free_rate = r,
+                                  delta_T = dt, 
+                                  max_maturity = T )
 
-E_Call = EuropeanCallOption(Strike = 48, Maturity = 2)
-E_Put = EuropeanPutOption(Strike = 48, Maturity = 2)
+E_Call = EuropeanCallOption(Strike = K, Maturity = T)
+E_Put = EuropeanPutOption(Strike = K, Maturity = T)
 
 E_Call_Value = BinomialMarketModel.priceOption(E_Call)
 E_Put_Value = BinomialMarketModel.priceOption(E_Put)
+
+# print(BinomialMarketModel.starting_node)
 
 print("European Call:", E_Call_Value)
 print("European Put :", E_Put_Value)
