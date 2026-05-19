0b788db132d188d5deaf7f7b36a9e12fa52184a7
FilipKowalski14 <filipkowalski1410@wp.pl>
2026-05-19 08:14:53 +0200

    replace full binary tree with recombining tree

--- MarketModel.py
+++ MarketModel.py
@@ -1,5 +1,4 @@
-from Tree import Node
-import numpy as np  
+import numpy as np
 
 class MarketModel:
     def __init__(self, SpotPrice, up, down, risk_free_rate, delta_T, max_maturity):
@@ -10,36 +9,30 @@ class MarketModel:
         self.delta_T = delta_T
         self.max_maturity = max_maturity
         self.p = (np.exp(self.risk_free_rate * self.delta_T) - self.down) / (self.up - self.down)
-        self.starting_node = Node(self.SpotPrice, layer=0)
+        self.steps = int(max_maturity / delta_T)
 
-        self.generatePriceTree()
+    def generate_terminal_prices(self):
+        prices = []
 
-    def generatePriceTree(self):
-        number_of_layers = self.max_maturity / self.delta_T
+        for i in range(self.steps + 1):
+            price = self.SpotPrice * (self.up ** (self.steps - i)) * (self.down ** i)
+            prices.append(price)
 
-        queue = [self.starting_node]
-        
-        while len(queue) > 0:
-            #print("Queue size: ", len(queue))
-            active_node = queue.pop(0)
-            #print("Generating tree node: ", active_node.underlying_price)
-            if active_node.layer >= number_of_layers:
-                continue
-            up_node = Node(active_node.underlying_price * self.up, layer=active_node.layer + 1)
-            down_node = Node(active_node.underlying_price * self.down, layer=active_node.layer + 1)
-            active_node.setUp(up_node)
-            active_node.setDown(down_node)
-            queue.append(up_node)
-            queue.append(down_node)
+    def priceOption(self, option):
+        terminal_prices = self.generate_terminal_prices()
 
-    def priceOption(self, option, node = None):
-        if node == None:
-            node = self.starting_node
-        if node.up == None and node.down == None:
-            return option.value(node.underlying_price)
-        if node.up == None or node.down == None:
-            raise Exception("Node with only one child missing")
-        
-        return np.exp(-self.risk_free_rate * self.delta_T) * \
-            (self.p * self.priceOption(option,node.up) + (1-self.p) * self.priceOption(option,node.down))
+        # dla kazdej koncowej ceny policz wartosc opcji
+        values = np.array([option.value(price) for price in terminal_prices])
+
+        for step in range(self.steps - 1, -1, -1):
+            new_values = np.zeros(step + 1)
+
+            for i in range(step + 1):
+                new_values[i] = np.exp(-self.risk_free_rate * self.delta_T) * (
+                    self.p * values[i] + (1 - self.p) * values[i + 1]
+                )
+
+            values = new_values
+
+        return values[0]
         
